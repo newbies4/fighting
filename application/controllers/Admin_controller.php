27a40038959191eb0fb1e744d42374e4c97436fa
Haidir Hassan <Studenthaidir@gmail.com>
2019-10-12 05:10:11 +0800

    Change controller names

--- application/controllers/Admin_controller.php
+++ application/controllers/Admin_controller.php
@@ -1,23 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Admin_controller extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
 		$data['main_view'] = "admin/dashboard";
@@ -107,38 +92,7 @@ class Home extends CI_Controller {
 		
 		$this->load->view('layouts/main', $data);
 	}
-	public function fetchMemberData() 
-	{
-		$result = array('data' => array());
-
-		$data = $this->model_member->fetchMemberData();
-		foreach ($data as $key => $value) {
-			$name = $value['fname'] . ' ' . $value['lname'];
-
-			// button
-			$buttons = '
-			<div class="btn-group">
-			  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			    Action <span class="caret"></span>
-			  </button>
-			  <ul class="dropdown-menu">
-			    <li><a type="button" onclick="editMember('.$value['id'].')" data-toggle="modal" data-target="#editMemberModal">Edit</a></li>
-			    <li><a type="button" onclick="removeMember('.$value['id'].')" data-toggle="modal" data-target="#removeMemberModal">Remove</a></li>			    
-			  </ul>
-			</div>
-			';
-
-			$result['data'][$key] = array(
-				$name,
-				$value['age'],
-				$value['contact'],
-				$value['address'],
-				$buttons
-			);
-		} // /foreach
-
-		echo json_encode($result);
-	}
+	
 
 
 }
---
+++ application/controllers/User_controller.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User_controller extends CI_Controller {
+
+	public function index()
+	{
+		$data['main_view'] = "user/home";
+
+		$this->load->view('layouts/main_user', $data);
+	}
+
+	public function user_account()
+	{
+		$data['main_view'] = "user/user_account";
+
+		$this->load->view('layouts/main_user', $data);
+	}
+
+
+	public function login()
+	{
+		$data['main_view'] = "user/login_user";
+
+		$this->load->view('layouts/main_user', $data);
+	}
+
+}
