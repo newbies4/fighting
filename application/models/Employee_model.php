ecbf2adcb7a08b2ed84fea46ec812c471ae9d103
Haidir Hassan <Studenthaidir@gmail.com>
2019-10-13 16:18:17 +0800

    add employee is working, added login form

--- application/models/Employee_model.php
+++ application/models/Employee_model.php
@@ -10,6 +10,19 @@ class Employee_model extends CI_Model {
 
 	}
 
+	public function insert_data($data) {
+
+
+		// $data = array(
+	 //        'first_name' => $data_array['firstname'],
+	 //        'middle_name' => $data_array['middlename'],
+	 //        'last_name' => $data_array['lastname'],
+	 //        'contact' => $data_array['contactnumber']
+		// );
+
+		$this->db->insert('tbl_employee', $data);
+	}
+
 }
 
 ?>
