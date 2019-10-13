a490748e428308d4c2c33dec3d3ed53f86acd73c
Haidir Hassan <Studenthaidir@gmail.com>
2019-10-13 10:46:52 +0800

    Stable version: removed many assets

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -15,7 +15,7 @@
     </nav>
 </div> -->
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
   <a class="navbar-brand" href="#">ZC CAR RENTAL</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -27,4 +27,23 @@
         <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">ADMIN</a>    
   <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">Settings</a><a class="dropdown-item" role="presentation" href="#">Log Out</a></div> 
   </div>
-</nav>
+</nav> -->
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css');?>" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/styles.css');?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/fonts/font-awesome.min.css');?>">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    
