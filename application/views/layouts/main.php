df5aa1eb6c2fe3853b6830387565536b2d5dedc4
Noob <euginiet@gmail.com>
2019-10-24 23:14:12 +0800

    minor changes on colors (admin side)

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -81,7 +81,7 @@
 
 <?php $this->load->view('layouts/header'); ?>
 <!-- Navigation Bar -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-0">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light mb-0">
     <a class="navbar-brand" href="#">ZC CAR RENTAL</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
