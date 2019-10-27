02cd7b65e99723290b98529da1e3e318fe77cd0e
Noob <euginiet@gmail.com>
2019-10-28 07:02:02 +0800

    login color button changed

--- application/views/admin/admin_login.php
+++ application/views/admin/admin_login.php
@@ -37,7 +37,7 @@
     </label>
     </div>
 
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    <button class="btn btn-lg btn-primary-login btn-block" type="submit">Sign in</button>
     <p class="mt-5 mb-3 text-muted">&copy; 2019</p>
 <?= form_close(); ?>
 
