75c939fd2a6c615ee182e82364684e3e17f84a38
Haidir Hassan <Studenthaidir@gmail.com>
2019-11-07 22:13:17 +0800

    New Database

--- car_rentaldb.sql
+++ car_rentaldb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 23, 2019 at 02:04 PM
+-- Generation Time: Nov 07, 2019 at 03:12 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.1.31
 
@@ -25,52 +25,46 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ci_sessions`
---
-
-CREATE TABLE `ci_sessions` (
-  `id` varchar(128) NOT NULL,
-  `ip_address` varchar(45) NOT NULL,
-  `timestamp` int(10) UNSIGNED NOT NULL DEFAULT 0,
-  `data` blob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ci_sessions`
---
-
-INSERT INTO `ci_sessions` (`id`, `ip_address`, `timestamp`, `data`) VALUES
-('bqb4eqegd82fdasno3emgf3uuoqb4dt7', '::1', 1571781815, 0x5f5f63695f6c6173745f726567656e65726174657c693a313537313738313831353b757365726e616d657c733a353a2261646d696e223b6c6f676765645f696e7c623a313b),
-('dh0g83qhm0ubrkn1lacsvlgii5495gjm', '::1', 1571781888, 0x5f5f63695f6c6173745f726567656e65726174657c693a313537313738313836393b757365726e616d657c733a353a2261646d696e223b6c6f676765645f696e7c623a313b),
-('h2h3k387ugojm9tvjj9mhmg9b2gsj3b7', '::1', 1571781263, 0x5f5f63695f6c6173745f726567656e65726174657c693a313537313738313236333b757365726e616d657c733a353a2261646d696e223b6c6f676765645f696e7c623a313b);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `tbl_car_profile`
 --
 
 CREATE TABLE `tbl_car_profile` (
   `car_id` int(11) NOT NULL,
-  `Lease` varchar(255) DEFAULT NULL,
   `car_owner` varchar(255) DEFAULT NULL,
-  `car_Unit_name` varchar(255) DEFAULT NULL,
-  `car_brand_name` varchar(255) DEFAULT NULL,
-  `car_year_model` varchar(255) DEFAULT NULL,
-  `car_seat` varchar(255) DEFAULT NULL,
-  `car_body_type` varchar(255) DEFAULT NULL,
-  `car_transmission` varchar(255) DEFAULT NULL,
-  `car_fuel_type` varchar(255) DEFAULT NULL,
-  `car_fuel_capacity` varchar(255) DEFAULT NULL,
+  `car_model` varchar(255) DEFAULT NULL,
+  `car_brand` varchar(255) DEFAULT NULL,
+  `car_type` varchar(255) DEFAULT NULL,
+  `car_seats` varchar(255) DEFAULT NULL,
   `car_color` varchar(255) DEFAULT NULL,
-  `car_interior_picture` varchar(255) DEFAULT NULL,
-  `car_exterior_picture_front` varchar(255) DEFAULT NULL,
-  `car_exterior_picture_back` varchar(255) DEFAULT NULL,
-  `car_exterior_picture_left` varchar(255) DEFAULT NULL,
-  `car_exterior_picture_right` varchar(255) DEFAULT NULL,
-  `insurance` varchar(255) DEFAULT NULL,
-  `lease_id` int(11) DEFAULT NULL
+  `car_platenumber` varchar(255) DEFAULT NULL,
+  `car_price` varchar(255) DEFAULT NULL,
+  `car_fuel_capacity` varchar(255) DEFAULT NULL,
+  `car_gas_type` varchar(255) DEFAULT NULL,
+  `car_driver` varchar(255) DEFAULT NULL,
+  `car_transmission` varchar(255) DEFAULT NULL,
+  `car_insurance` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_car_profile`
+--
+
+INSERT INTO `tbl_car_profile` (`car_id`, `car_owner`, `car_model`, `car_brand`, `car_type`, `car_seats`, `car_color`, `car_platenumber`, `car_price`, `car_fuel_capacity`, `car_gas_type`, `car_driver`, `car_transmission`, `car_insurance`, `created_at`) VALUES
+(1, 'Loremipsum', 'Loremipsum', 'Loremipsum', 'Loremipsum', '4', 'Loremipsum', 'Loremipsum', '1', 'Loremipsum', 'Diesel', 'With Driver', 'manual', 'ctp', '2019-11-05 21:52:00'),
+(2, 'Loremipsum', 'Loremipsum', 'Loremipsum', 'Loremipsum', '4', 'Loremipsum', 'Loremipsum', '1', 'Loremipsum', 'Diesel', 'With Driver', 'manual', 'ctp', '2019-11-05 21:53:37'),
+(3, 'Loremipsum', 'Loremipsum', 'Loremipsum', 'Loremipsum', '4', 'Loremipsum', 'Loremipsum', '1', 'Loremipsum', 'Diesel', 'With Driver', 'manual', 'ctp', '2019-11-05 21:54:06'),
+(4, 'Loremipsum', 'Loremipsum', 'Loremipsum', 'Loremipsum', '4', 'Loremipsum', 'Loremipsum', '1', 'Loremipsum', 'Diesel', 'With Driver', 'manual', 'ctp', '2019-11-05 21:54:28'),
+(5, 'Loremipsumaw', 'Loremipsum', 'Loremipsum', 'Loremipsum', '4', 'Loremipsum', 'Loremipsum', '1', 'Loremipsum', 'Diesel', 'With Driver', 'manual', 'ctp', '2019-11-05 21:54:46'),
+(6, 'Loremipsumddddd', 'Loremipsum', 'Loremipsum', 'Loremipsum', '4', 'Loremipsum', 'Loremipsum', '1', 'Loremipsum', 'Diesel', 'With Driver', 'manual', 'ctp', '2019-11-05 22:03:13'),
+(7, 'Loremipsumddddd', 'Loremipsum', 'Loremipsum', 'Loremipsum', '4', 'Loremipsum', 'Loremipsum', '1', 'Loremipsum', 'Diesel', 'With Driver', 'manual', 'ctp', '2019-11-05 22:04:28'),
+(8, 'Loremipsumddddd', 'Loremipsum', 'Loremipsum', 'Loremipsum', '4', 'Loremipsum', 'Loremipsum', '1', 'Loremipsum', 'Diesel', 'With Driver', 'manual', 'ctp', '2019-11-05 22:08:16'),
+(9, 'Loremipsum', 'Loremipsum', 'Loremipsum', 'Loremipsum', '4', 'Loremipsum', 'Loremipsum', '1', 'Loremipsum', 'Diesel', 'With Driver', 'manual', 'ctp', '2019-11-05 22:10:25'),
+(10, 'Loremipsum', 'Loremipsum', 'Loremipsum', 'Loremipsum', '4', 'Loremipsum', 'Loremipsum', '1', 'Loremipsum', 'Diesel', 'With Driver', 'manual', 'ctp', '2019-11-05 22:13:40'),
+(11, 'Loremipsum', 'Loremipsum', 'Loremipsum', 'Loremipsum', '4', 'Loremipsum', 'Loremipsum', '1', 'Loremipsum', 'Diesel', 'With Driver', 'manual', 'ctp', '2019-11-05 22:14:06'),
+(12, 'Loremipsum', 'Loremipsum', 'Loremipsum', 'Loremipsum', '4', 'Loremipsum', 'Loremipsum', '1', 'Loremipsum', 'Diesel', 'With Driver', 'manual', 'ctp', '2019-11-05 22:14:46'),
+(13, 'Loremipsumlllll', 'Loremipsum', 'Loremipsum', 'Loremipsum', '4', 'Loremipsum', 'Loremipsum', '1', 'Loremipsum', 'Diesel', 'With Driver', 'manual', 'ctp', '2019-11-05 22:15:14'),
+(14, 'Loremipsum', 'Loremipsum', 'Loremipsum', 'Loremipsum', '4', 'Loremipsum', 'Loremipsum', '1', 'Loremipsum', 'Diesel', 'With Driver', 'manual', 'ctp', '2019-11-07 02:44:15');
 
 -- --------------------------------------------------------
 
@@ -142,8 +136,17 @@ INSERT INTO `tbl_employee` (`id`, `first_name`, `middle_name`, `last_name`, `con
 (14, 'Test', 'Test', 'Test', '11111111111', '2019-10-13 07:20:39'),
 (15, 'Test', 'Test', 'Test', '22222222222', '2019-10-13 07:20:50'),
 (16, 'Test', 'Test', 'Test', '33333333333', '2019-10-13 07:21:36'),
-(17, 'Test', 'Test', 'Test', '44444444444', '2019-10-13 09:05:09'),
-(18, 'jumong', 'asdfad', 'adsf', '0123123213', '2019-10-15 09:08:29');
+(17, 'jumong', 'Test', 'Test', '44444444444', '2019-10-13 09:05:09'),
+(18, 'jumong', 'asdfad', 'adsf', '0123123213', '2019-10-15 09:08:29'),
+(19, 'aaaaaaaaaa', 'bbbbbbbbb', 'bbbbbbbbb', '123123213', '2019-10-26 06:14:55'),
+(23, 'asdfsadf', '123123', 'jadfjhasfj', '1231242183', '2019-10-26 10:38:10'),
+(24, 'hfdhgh', 'dfhfd', 'ghfdhdfh', '2313123123', '2019-10-26 10:38:22'),
+(25, 'asdasdasdfasf', 'sdafasf', 'asdfsdf', '123123123', '2019-10-26 10:38:47'),
+(26, 'dsfgdsgdsg', 'dsfgfdgfsfdg', 'dfsgdfsgsdfg', '234234234', '2019-10-26 10:38:52'),
+(27, 'fshdfgdfgsd', 'dsfgsdgdfg', 'dsfgdgsdfg', '12312312312213', '2019-10-26 10:38:58'),
+(28, 'asdfasdf', 'asdfsadfd', 'fdsafasf', '575757788', '2019-10-26 12:05:45'),
+(29, 'asdasd', 'asdasd', 'asdasd', '123123123213', '2019-10-27 14:21:57'),
+(30, 'ppppppppppppppppp', 'ppppppppppppppppppp', 'pppppppppppppppppp', '1111111111111', '2019-10-27 14:22:26');
 
 -- --------------------------------------------------------
 
@@ -198,27 +201,16 @@ INSERT INTO `tbl_useraccount` (`user_id`, `user_type`, `username`, `password`) V
 --
 
 --
--- Indexes for table `ci_sessions`
---
-ALTER TABLE `ci_sessions`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `ci_sessions_timestamp` (`timestamp`);
-
---
 -- Indexes for table `tbl_car_profile`
 --
 ALTER TABLE `tbl_car_profile`
-  ADD PRIMARY KEY (`car_id`),
-  ADD KEY `lease_id` (`lease_id`);
+  ADD PRIMARY KEY (`car_id`);
 
 --
 -- Indexes for table `tbl_car_rent`
 --
 ALTER TABLE `tbl_car_rent`
-  ADD PRIMARY KEY (`car_rent_id`),
-  ADD KEY `customer_id` (`customer_id`),
-  ADD KEY `car_id` (`car_id`),
-  ADD KEY `reserve_id` (`reserve_id`);
+  ADD PRIMARY KEY (`car_rent_id`);
 
 --
 -- Indexes for table `tbl_customer`
@@ -243,9 +235,7 @@ ALTER TABLE `tbl_lease`
 -- Indexes for table `tbl_reserve`
 --
 ALTER TABLE `tbl_reserve`
-  ADD PRIMARY KEY (`reserve_id`),
-  ADD KEY `customer_id` (`customer_id`),
-  ADD KEY `car_id` (`car_id`);
+  ADD PRIMARY KEY (`reserve_id`);
 
 --
 -- Indexes for table `tbl_useraccount`
@@ -261,7 +251,7 @@ ALTER TABLE `tbl_useraccount`
 -- AUTO_INCREMENT for table `tbl_car_profile`
 --
 ALTER TABLE `tbl_car_profile`
-  MODIFY `car_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `car_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `tbl_car_rent`
@@ -279,7 +269,7 @@ ALTER TABLE `tbl_customer`
 -- AUTO_INCREMENT for table `tbl_employee`
 --
 ALTER TABLE `tbl_employee`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `tbl_lease`
@@ -304,31 +294,10 @@ ALTER TABLE `tbl_useraccount`
 --
 
 --
--- Constraints for table `tbl_car_profile`
---
-ALTER TABLE `tbl_car_profile`
-  ADD CONSTRAINT `tbl_car_profile_ibfk_1` FOREIGN KEY (`lease_id`) REFERENCES `tbl_lease` (`lease_id`);
-
---
--- Constraints for table `tbl_car_rent`
---
-ALTER TABLE `tbl_car_rent`
-  ADD CONSTRAINT `tbl_car_rent_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `tbl_customer` (`customer_id`),
-  ADD CONSTRAINT `tbl_car_rent_ibfk_2` FOREIGN KEY (`car_id`) REFERENCES `tbl_car_profile` (`car_id`),
-  ADD CONSTRAINT `tbl_car_rent_ibfk_3` FOREIGN KEY (`reserve_id`) REFERENCES `tbl_reserve` (`reserve_id`);
-
---
 -- Constraints for table `tbl_customer`
 --
 ALTER TABLE `tbl_customer`
   ADD CONSTRAINT `tbl_customer_ibfk_1` FOREIGN KEY (`user_id_fk`) REFERENCES `tbl_useraccount` (`user_id`);
-
---
--- Constraints for table `tbl_reserve`
---
-ALTER TABLE `tbl_reserve`
-  ADD CONSTRAINT `tbl_reserve_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `tbl_customer` (`customer_id`),
-  ADD CONSTRAINT `tbl_reserve_ibfk_2` FOREIGN KEY (`car_id`) REFERENCES `tbl_car_profile` (`car_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
