4dbe17d745cb0287049af5ac3c2a3e1f903778cd
arczhar <arczharjhet17@gmail.com>
2019-11-09 18:57:43 +0800

    little adjustment of breadcrumbs in add cars

--- application/views/admin/add_car_finalize.php
+++ application/views/admin/add_car_finalize.php
@@ -1,9 +1,13 @@
+<header>
+  <center><h3>ADD CARS  </h3></center>
+  <br>
+</header>
 <!--breadcrumb -->
 
 <nav aria-label="breadcrumb">
   <ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="#">1) Car Information</a></li>
-    <li class="breadcrumb-item" ><a href="#">2) Add Car Photos</a></li>
+    <li class="breadcrumb-item"><a href="<?php echo base_url('admin_controller/add_car');?>">1) Car Information</a></li>
+    <li class="breadcrumb-item" ><a href="<?php echo base_url('admin_controller/add_car_picture');?>">2) Add Car Photos</a></li>
 	<li class="breadcrumb-item active" aria-current="page"><a href="#">3) Review</a></li>
   </ol>
 </nav>
--- application/views/admin/add_cars.php
+++ application/views/admin/add_cars.php
@@ -1,19 +1,20 @@
+<header>
+  <center><h3>ADD CARS  </h3></center>
+  <br>
+</header>
 <!--breadcrumb -->
 
 <nav aria-label="breadcrumb">
   <ol class="breadcrumb">
     <li class="breadcrumb-item active" aria-current="page"><a href="#">1) Car Information</a></li>
-    <li class="breadcrumb-item"><a href="#">2) Add Car Photos</a></li>
+    <li class="breadcrumb-item"><a href="<?php echo base_url('admin_controller/add_car_picture');?>">2) Add Car Photos</a></li>
 	<li class="breadcrumb-item"><a href="#">3) Review</a></li>
   </ol>
 </nav>
 
 
 <!--end of breadcrumb -->
-<header>
-  <center><h3>ADD CARS  </h3></center>
-  <br>
-</header>
+
 
 <div class="container">
 	<?php echo validation_errors('<div class="text-danger">','</div>'); ?>
