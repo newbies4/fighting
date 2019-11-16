2a0366a178675bd236ecbe23ce44d02c303d0236
Jhet <arczharjhet17@gmail.com>
2019-11-16 15:37:24 +0800

    view cars ui

--- application/views/user/user_car_details.php
+++ application/views/user/user_car_details.php
@@ -1,1 +1,55 @@
-ddd
+<br>
+<br>
+<br>
+<br>
+<div class="container">
+<div class="profile-content">
+	<div class="wrapper row">
+		<div class="preview col-sm-4">
+						
+		    <div class="preview-pic tab-content">
+			    <div class="tab-pane active" id="pic-1"><img src=<?php echo base_url("assets/Content-filter/home-img/img-1.jpg" )?> /></div>
+				<div class="tab-pane" id="pic-2"><img src=<?php echo base_url("assets/Content-filter/home-img/img-1.1.jpg")?> /></div>
+				<div class="tab-pane" id="pic-3"><img src=<?php echo base_url("assets/Content-filter/home-img/img-1.jpg")?> /></div>
+			</div>
+            
+            <ul class="preview-thumbnail nav nav-tabs">
+				<li class="active"><a data-target="#pic-1" data-toggle="tab"><img src=<?php echo base_url("assets/Content-filter/home-img/img-1.jpg")?> /></a></li>
+				<li><a data-target="#pic-2" data-toggle="tab"><img src=<?php echo base_url("assets/Content-filter/home-img/img-1.1.jpg")?> /></a></li>
+				<li><a data-target="#pic-3" data-toggle="tab"><img src=<?php echo base_url("assets/Content-filter/home-img/img-1.jpg")?> /></a></li>
+			</ul>				
+		</div>
+        
+        <div class="details col-sm-4">
+            <h3 class="product-title">Car Name</h3>
+            <p><strong>Year Model:</strong> 2016</p>
+            <p><strong>Brand:</strong> Car Brand</p>
+            <p><strong>Seats:</strong> 00</p>
+            <p><strong>Transmission:</strong> transmission</p>
+            <p><strong>Fuel Type:</strong> fuel type</p>
+            <p><strong>Body Type:</strong> body type</p>
+            <br>
+            <h4 class="price">Price: <span>00.00</span></h4>
+
+			
+        </div>
+        
+        <div class="details col-sm-4">
+        <div class="action">
+        <br>
+        <h5> Select Date of Rent </h5>
+        <br>
+        <label >From</label>
+                  		<input type="date" name="from" max="3000-12-31" 
+                          min="1000-01-01" class="form-control">
+        <br>
+        <label >To</label>
+                  		<input type="date" name="from" max="3000-12-31" 
+                          min="1000-01-01" class="form-control">
+					
+				<button class="add-to-storeroom btn btn-default custom-position" type="button">Add to Storeroom</button>
+			</div>
+        </div>
+    </div>
+</div>
+</div>
